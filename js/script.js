0e21aff6948be40034cdc72804650e5a33ebb663
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 09:19:23 +0100

    aggiunta ora attuale in js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready (function(){
   var messaggioUtente = $("#messaggio_utente_template");
   var input = $("#input_messaggio");
+  var data = new Date();
+  var ora = data.getHours() + ":" + data.getMinutes();
+  // var clone = $("#messaggio_utente_template.").clone();
+  // console.log(clone);
+  // $('#main_content').append(clone);
   // messaggioUtente.children('p').text('ciao');
   // messaggioUtente.children('span').text('11:20');
 
-  $(".fas.fa-microphone").on( "click", function( event ) {
-  messaggioUtente.children('p').text( input.val());
+  $(".fas.fa-microphone").on( "click", function() {
+  messaggioUtente.children('p').text(input.val());
+  messaggioUtente.children('span').text(ora);
 })
 
 });
