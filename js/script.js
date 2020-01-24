3f6bbf34fc6c93619558054e648dccaf7ac5490b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 17:00:50 +0100

    fixato problema

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
 function sendMessage() {
   var input = $("#input_messaggio").val();
   if (input.length != 0){
-  var nuovoMessaggio = $(".template.messaggio").clone();
+    var nuovoMessaggio = $(".template > .messaggio").clone();
 
-  nuovoMessaggio.find(".testo_messaggio").text(input);
-  nuovoMessaggio.find(".ora_messaggio").text('10:30');
-  nuovoMessaggio.addClass('inviato');
-  $('.main_content.active').append(nuovoMessaggio);
-}
+    nuovoMessaggio.find(".testo_messaggio").text(input);
+    nuovoMessaggio.find(".ora_messaggio").text('10:30');
+    nuovoMessaggio.addClass('inviato');
+    $('.main_content.active').append(nuovoMessaggio);
+  }
 }
