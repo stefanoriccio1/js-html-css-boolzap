dc04f561389220d6d18c29464336f0fe09ab0e53
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 07:38:58 +0100

    iniziato JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+var messaggioUtente = $("#messaggio_utente_template");
+messaggioUtente.children('p').text('ciao'),
+messaggioUtente.children('span').text('11:20')
 
+
+// funzioni
+function inserimentoMessaggio (){
+ messaggioUtente.text('ciao come va?')
+}
