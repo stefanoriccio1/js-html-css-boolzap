8e79dc27c96b17dcb139b38723be3bd9b5092a94
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 08:36:22 +0100

    tentativi js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-var messaggioUtente = $("#messaggio_utente_template");
-messaggioUtente.children('p').text('ciao'),
-messaggioUtente.children('span').text('11:20')
-
-
-// funzioni
-function inserimentoMessaggio (){
- messaggioUtente.text('ciao come va?')
-}
+$(document).ready (function(){
+  var messaggioUtente = $("#messaggio_utente_template");
+  // messaggioUtente.children('p').text('ciao');
+  // messaggioUtente.children('span').text('11:20');
+  $( "#input_messaggio" ).on( "keypress", function( event ) {
+  messaggioUtente.children('p').html( event.type + ": " +  event.which );
+})
+});
