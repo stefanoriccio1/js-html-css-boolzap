fd379ff1222ae87793d70cdd2651afbb81a72b60
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 08:48:18 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready (function(){
   var messaggioUtente = $("#messaggio_utente_template");
+  var input = $("#input_messaggio");
   // messaggioUtente.children('p').text('ciao');
   // messaggioUtente.children('span').text('11:20');
-  $( "#input_messaggio" ).on( "keypress", function( event ) {
-  messaggioUtente.children('p').html( event.type + ": " +  event.which );
+
+  $(".fas.fa-microphone").on( "click", function( event ) {
+  messaggioUtente.children('p').text( input.val());
 })
+
 });
