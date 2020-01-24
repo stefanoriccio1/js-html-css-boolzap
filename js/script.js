2e34bd3571a326dd63cd29dc3bb4735981ac0010
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-24 16:23:16 +0100

    ricominciata impostazione JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-$(document).ready (function(){
-  var messaggioUtente = $(".messaggio_utente_template");
-  var input = $("#input_messaggio");
-  var data = new Date();
-  var ora = data.getHours() + ":" + data.getMinutes();
-  // var clone = $("#messaggio_utente_template.").clone();
-  // console.log(clone);
-  // $('#main_content').append(clone);
-  // messaggioUtente.children('p').text('ciao');
-  // messaggioUtente.children('span').text('11:20');
-
-  $(".fas.fa-microphone").on( "click", function() {
-  messaggioUtente.children('p').text(input.val());
-  messaggioUtente.children('span').text(ora);
-})
-
-$(".lista_messaggi li").on("click", function(){
-  $(this).remove();
-})
+$(document).ready(function () {
+  $('.icona_bottom.invio').click(function () {
+    sendMessage();
+  });
 });
+
+function sendMessage() {
