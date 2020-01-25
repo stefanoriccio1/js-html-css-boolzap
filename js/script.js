a5af1b20e3b9eb9c07a45948799be05ebd6be902
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-25 16:03:07 +0100

    Modificata funzione di input

--- js/script.js
+++ js/script.js
@@ -8,10 +8,13 @@ function sendMessage() {
   var input = $("#input_messaggio").val();
   if (input.length != 0){
     var nuovoMessaggio = $(".template > .messaggio").clone();
-
+    var data = new Date();
+    var orario = data.getHours() + ':' + data.getMinutes();
     nuovoMessaggio.find(".testo_messaggio").text(input);
-    nuovoMessaggio.find(".ora_messaggio").text('10:30');
+    nuovoMessaggio.find(".ora_messaggio").text(orario);
     nuovoMessaggio.addClass('inviato');
     $('.main_content.active').append(nuovoMessaggio);
+
+    $("#input_messaggio").val('');
   }
 }
