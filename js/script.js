a5e09ebc3dfabee848b5e04c0d5c154b4950c870
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-27 15:37:58 +0100

    Aggiunta funzione per la cancellazione dei mex

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
       sendMessage();
     }
   });
+  // funzione per trovare le chat corrispondenti ad un determinato utente
   $('.lista_chat li').click(selezioneChat);
 
   $('.barra_ricerca').keyup(function(){
@@ -24,6 +25,7 @@ $(document).ready(function () {
     })
   });
 
+ // funzione di apertura menu messaggio
   $(document).on('click', '.opzioni_messaggio', function(){
     var finestraMessaggio = $(this).parent().find('.menu_messaggio');
 
@@ -33,10 +35,16 @@ $(document).ready(function () {
     else{
       finestraMessaggio.addClass('hidden');
     }
-  })
+  });
+  // funzione per elminare messaggio cliccando "cancella messaggio"
+  $(document).on('click', '.cancella', function(){
+    console.log('ciao');
+    $(this).parents()[2].remove();
+  });
 });
 
 // funzioni
+// funzione di invio messaggio e del messaggio di rispota
 function sendMessage() {
   var input = $("#input_messaggio").val();
   if (input.length != 0){
@@ -50,7 +58,7 @@ function sendMessage() {
 
     $("#input_messaggio").val('');
     setTimeout(risposta, 2000);
-  }
+  };
 
   function risposta(){
     var messaggioDiRisposta = $('.template > .messaggio').clone();
@@ -60,12 +68,13 @@ function sendMessage() {
     messaggioDiRisposta.find('.ora_messaggio').text(orario);
     messaggioDiRisposta.addClass('ricevuto');
     $('.main_content.active').append(messaggioDiRisposta);
-  }
+  };
 }
 
+// funzione per cambiare avatar e nome nella barra superiore del main container
 function selezioneChat(){
   var mainContents = $('.main_content');
-  // console.log(mainContents [0]);
+
   mainContents.removeClass('active');
   $(mainContents[$(this).attr('data-contact')]).addClass('active');
 
@@ -75,4 +84,4 @@ function selezioneChat(){
 
   var nomeCorrente = $(this).find('p.bold').text();
   $('.nome_utente_chat p.nome_utente').text(nomeCorrente);
-  }
+};
